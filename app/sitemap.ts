721a8a9ae165b673a034247d4f579b8e19474ef4
jomefavourite <jfjomefavourite@gmail.com>
2023-06-05 15:00:07 +0100

    removed base route from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,6 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://favourite.site/',
-      lastModified: new Date(),
-    },
-    {
       url: 'https://www.favouritejome.site/projects',
       lastModified: new Date(),
     },
