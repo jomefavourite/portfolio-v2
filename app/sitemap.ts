d6f2b3236c5e79ce6eea75f7c50c7a517c9641d4
jomefavourite <jfjomefavourite@gmail.com>
2023-06-05 14:54:51 +0100

    update to site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://favourite.site',
+      url: 'https://favourite.site/',
       lastModified: new Date(),
     },
     {
