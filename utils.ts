66f6618d5fbc2d07484a411d0500d9430b37e170
jomefavourite <jfjomefavourite@gmail.com>
2023-08-23 17:47:59 +0100

    remove runtech space

--- utils.ts
+++ utils.ts
@@ -20,89 +20,89 @@ export type Socials = {
 
 export const navigation: Nav[] = [
   {
-    href: "/",
-    title: "Home",
+    href: '/',
+    title: 'Home',
   },
   // {
   //   href: "/about",
   //   title: "About",
   // },
   {
-    href: "/projects",
-    title: "Projects",
+    href: '/projects',
+    title: 'Projects',
   },
   {
-    href: "/articles",
-    title: "Articles",
+    href: '/articles',
+    title: 'Articles',
   },
   // {
   //   href: "/resume",
   //   title: "Resume",
   // },
   {
-    href: "/experiences",
-    title: "Experiences",
+    href: '/experiences',
+    title: 'Experiences',
   },
 ];
 
 export const pinnedProjects: Project[] = [
   {
-    title: "AlgoVisual",
+    title: 'AlgoVisual',
     description:
-      "Algorithm Visualizer for Sorting, Searching and Graph Traversal algorithms",
-    live: "https://jf-algo-visual.vercel.app/",
-    github: "https://github.com/jomefavourite/algo-visual",
-    tags: ["Nextjs", "Typescript", "Tailwindcss"],
+      'Algorithm Visualizer for Sorting, Searching and Graph Traversal algorithms',
+    live: 'https://jf-algo-visual.vercel.app/',
+    github: 'https://github.com/jomefavourite/algo-visual',
+    tags: ['Nextjs', 'Typescript', 'Tailwindcss'],
   },
   {
-    title: "PlanDone",
+    title: 'PlanDone',
     description:
-      "PlanDone is made for Students who would like to learn smart, be productive and access everything they need while studying in one place.",
-    live: "https://plandone.onrender.com/",
-    github: "https://github.com/jomefavourite/PlanDone",
-    tags: ["Node.js", "Express", "Ejs", "MongoDB", "SASS"],
+      'PlanDone is made for Students who would like to learn smart, be productive and access everything they need while studying in one place.',
+    live: 'https://plandone.onrender.com/',
+    github: 'https://github.com/jomefavourite/PlanDone',
+    tags: ['Node.js', 'Express', 'Ejs', 'MongoDB', 'SASS'],
   },
   {
-    title: "CGPA-Result-Forecaster",
+    title: 'CGPA-Result-Forecaster',
     description:
-      "Calculate your GPA and CGPA score and from your score get the average estimate score to end up with a 1st, 2nd upper/lower class.",
-    live: "https://cgpaforecaster.vercel.app/",
-    github: "https://github.com/jomefavourite/CGPA-Result-Forecaster",
-    tags: ["JavaScript", "gpa-calculator", "cgpa-forecast"],
+      'Calculate your GPA and CGPA score and from your score get the average estimate score to end up with a 1st, 2nd upper/lower class.',
+    live: 'https://cgpaforecaster.vercel.app/',
+    github: 'https://github.com/jomefavourite/CGPA-Result-Forecaster',
+    tags: ['JavaScript', 'gpa-calculator', 'cgpa-forecast'],
   },
 ];
 
 export const otherProjects: Project[] = [
   {
-    title: "Faraday Event",
+    title: 'Faraday Event',
     description: `Event Manger App for Faraday Africa Community. Co-created with`,
-    live: "https://events.faraday.africa/",
-    github: "",
-    tags: ["ReactJS", "Airtable", "Tailwindcss"],
-    desLink: "https://github.com/nworiekingslee",
+    live: 'https://events.faraday.africa/',
+    github: '',
+    tags: ['ReactJS', 'Airtable', 'Tailwindcss'],
+    desLink: 'https://github.com/nworiekingslee',
   },
+  // {
+  //   title: "RunTechSpace",
+  //   description:
+  //     "A community platform for tech enthusiasts at Redeemer's University",
+  //   live: "https://runtechspace.com/",
+  //   github: "",
+  //   tags: ["Nextjs", "Tailwindcss"],
+  // },
   {
-    title: "RunTechSpace",
-    description:
-      "A community platform for tech enthusiasts at Redeemer's University",
-    live: "https://runtechspace.com/",
-    github: "",
-    tags: ["Nextjs", "Tailwindcss"],
-  },
-  {
-    title: "Faraday Africa",
+    title: 'Faraday Africa',
     description:
       "Faraday is a Community driven Ed-tech solution improving the learning experience for Students in tertiary institutions. I'm one of the contributors, building the platform.",
-    live: "https://www.faraday.africa/",
-    github: "https://github.com/faradayafrica/Faraday-frontend",
-    tags: ["ReactJS", "Airtable", "Tailwindcss"],
+    live: 'https://www.faraday.africa/',
+    github: 'https://github.com/faradayafrica/Faraday-frontend',
+    tags: ['ReactJS', 'Airtable', 'Tailwindcss'],
   },
 
   {
-    title: "Portfolio V2",
-    description: "Hey, this is my portfolio",
-    live: "",
-    github: "https://github.com/jomefavourite/portfolio-v2",
-    tags: ["Nextjs", "Typescript", "Tailwindcss"],
+    title: 'Portfolio V2',
+    description: 'Hey, this is my portfolio',
+    live: '',
+    github: 'https://github.com/jomefavourite/portfolio-v2',
+    tags: ['Nextjs', 'Typescript', 'Tailwindcss'],
   },
 ];
