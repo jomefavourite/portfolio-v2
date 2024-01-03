dcb28b15e7e629ed532fb92f3ea0de4d15a3948f
Favourite Jome <54733225+jomefavourite@users.noreply.github.com>
2024-01-03 12:42:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://blog-woad-six-17.vercel.app/blog"
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://blog-woad-six-17.vercel.app/blog/:path*"
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
